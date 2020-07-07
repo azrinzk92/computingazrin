513d606203a7609c99addb09ed0bd6d6c437fba2
azrinzk92 <67951414+azrinzk92@users.noreply.github.com>
2020-07-07 22:14:33 +0800

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '-&)qj9c_!mx^n0fhyb*(44c&uvsto=21w4x9_7g4zq)a(@e2df'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://computingazrin.herokuapp.com/', '127.0.0.1']
 
 
 # Application definition
